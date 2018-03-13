c456af921e3bb57d0e1937b5cb4a96f4780e0b1f
winnerwly <975080391@qq.com>
2018-03-13 14:00:14 +0800

    ..

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,9 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  	$('.mybtn').click(()=>{
+  		alert(1)
+  	})
   }
 
 }
